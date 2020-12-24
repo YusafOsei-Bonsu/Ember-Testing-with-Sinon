db013eadf0acaa8a7f02217001382c6a829d02c6
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2020-12-24 15:25:28 +0000

    Completing Controller code

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,28 @@
 import Controller from '@ember/controller';
+import { A } from '@ember/array'; 
 
 export default Controller.extend({
+    buttonGroups: A([
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9'],
+        ['*', '0', '#'],
+    ]),
+    currentNumber: '',
+    // Erases the current phone number
+    clearNumber: function() {
+        this.set('currentNumber', '');
+    },
+    dialNumber: function() {
+        // Dial the number
+        this.clearNumber();
+    },
+    actions: {
+        receiveButtonPress: function(buttonId) {
+            this.set('currentNumber', this.get('currentNumber') + buttonId);
+
+            // If the number is valid, dial the number
+            if (this.get('currentNumber').length === 10) this.dialNumber();
+        }
+    }
 });
