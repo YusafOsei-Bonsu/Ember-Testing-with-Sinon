167d064ffd6e8daae1497ae61138fdfd366d8c86
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2020-12-24 15:26:28 +0000

    buttonPressed action for button-with-id component

--- app/components/button-with-id.js
+++ app/components/button-with-id.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    actions: {
+        buttonPressed: function() {
+            this.sendAction('sendId', this.get('id'));
+        }
+    }
 });
